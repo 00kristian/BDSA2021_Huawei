04cebe63f7a3ccc12c100a6f8fce1d23760f5326
luhj <luhj@itu.dk>
2021-11-15 16:05:55 +0100

    Temporary SQLLite Database setup

---
+++ EF_ProjectBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace EF_PB;
+internal class Program
+{
+    private static void Main()
+    {
+        using (var db = new ProjectBankContext())
+        {
+            Console.WriteLine($"Database path: {db.DbPath}.");
+            
+            // Create
+            Console.WriteLine("Inserting a new project");
+            db.projects.Add(new Project{Name = "p1"});
+            db.SaveChanges();
+
+            // Read
+            Console.WriteLine("Querying for a project");
+            var p = db.projects
+                .First();
+            System.Console.WriteLine(p.Name);
+        }
+    }
+}
