2384a2778830cf53cbd851d899fa2967a172cc68
luhj <luhj@itu.dk>
2021-11-26 13:33:37 +0100

    swagger

--- Server/Program.cs
+++ Server/Program.cs
@@ -29,12 +29,13 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
     app.UseDeveloperExceptionPage();
     app.UseWebAssemblyDebugging();
 }
