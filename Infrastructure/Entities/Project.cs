d005c63a26c30a0980e48b5afbfb09a7d959de3c
isabellaharriesmagnusdottir <imag@itu.dk>
2021-12-06 00:42:02 +0100

    Påbegyndelse af strukturering af database. Ikke rørt DTOs endnu. Kun keyword og project :)

--- Infrastructure/Entities/Project.cs
+++ Infrastructure/Entities/Project.cs
@@ -12,16 +12,11 @@ namespace Infrastructure
         public string? Description { get; set; }       
         public DateTime DueDate { get; set; }
         public int IntendedWorkHours { get; set; }
-        //public Language Language { get; set; }        
-       
-        //public ISet<string>? Keywords { get; set; }
+        public Language Language { get; set; }               
         public string? SkillRequirementDescription { get; set; }
-        
         public string? SupervisorName { get; set; }
-        
-        //public ISet<string>? WorkDays { get; set; }
-        
-        //public Location Location { get; set; }        
+        public Location Location { get; set; }        
         public bool isThesis { get; set; }
+        public ICollection<Keyword> Keywords {get;set;}
     }
 }
